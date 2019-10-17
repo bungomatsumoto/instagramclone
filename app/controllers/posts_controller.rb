e3d693e0bc6097a1f9e611574f0e98d7dc7c8d0f
bungo matsumoto <matsumotobg@gmail.com>
2019-10-17 13:26:34 +0900

    削除画面を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :get_id_post, only: [:show, :edit, :update]
-  
+  before_action :get_id_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -33,6 +33,12 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+
+    def destroy
+      @post.destroy
+      redirect_to posts_path "投稿を削除しました"
+    end
+
   end
 
   private
