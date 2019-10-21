d150bcb96c6f37b3f1d21489f3ce5f357685d9da
bungo matsumoto <matsumotobg@gmail.com>
2019-10-21 22:52:04 +0900

    letter_opener_webを実装

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,7 +1,8 @@
 class PostMailer < ApplicationMailer
 
-  def send_when_post(user)
-    @user = user
+  def send_when_post(post)
+    # @user = user
+    @post = post
     mail to: "hw.bm0122.wc@gmail.com", subject: "投稿の確認"
   end
 end
